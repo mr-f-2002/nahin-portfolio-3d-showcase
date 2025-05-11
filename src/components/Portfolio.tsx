f7880d033d5975c9c1fcf6cb31d923b6a208922d
Nahin F. Siddiqui <nahinsiddiqui@iut-dhaka.edu>
2025-05-11 13:05:47 +0600

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -65,6 +65,14 @@ const Portfolio = () => {
       description: "A health-tracking mobile app with personalized fitness recommendations.",
       technologies: ["React-native", "Firebase"],
       image: "placeholder"
+    },
+    {
+      id: 7,
+      title: "Event Planner",
+      description: "A python based simple daily dashboard that keeps track of todo, daily work progress and reminders",
+      technologies: ["Python", "Tkinter"],
+      image: "placeholder"
+      link: "https://github.com/mr-f-2002/event-planner-dashboard.git"
     }
   ];
   
