0f0d5ed9555136878239bbd3393b61b1cd1a823e
Nahin F. Siddiqui <nahinsiddiqui@iut-dhaka.edu>
2025-05-13 00:11:17 +0600

    reverting the attempt

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -40,7 +40,7 @@ const Portfolio = () => {
       title: "PoorCAD",
       description: "A simple drawing app with multiple shapes and pen tools.",
       technologies: ["C++", "SFML"],
-      image: "https://i.postimg.cc/XqVkgHvb/poorcad-2.png",
+      image: "placeholder",
       link: "https://github.com/mr-f-2002/Poor-Cad"
     },
     {
