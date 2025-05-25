ab2fa01b28ac8cc399bf7d90eaffdbf4165c3c17
Nahin F. Siddiqui <nahinsiddiqui@iut-dhaka.edu>
2025-05-25 15:55:35 +0600

    update image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -91,7 +91,7 @@ const Hero = () => {
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div className="w-72 h-72 md:w-96 md:h-96 relative">
                   <img
-                    src="https://i.postimg.cc/MGyyvKTS/image-1.webp"
+                    src="https://i.postimg.cc/Mp07GpX8/image-1.png"
                     alt="Nahin"
                     className="w-full h-full object-cover object-center"
                     style={{
