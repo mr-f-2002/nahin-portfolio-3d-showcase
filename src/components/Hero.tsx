14428f366ae8c97c9f90472d7b8fdb75a460e12d
Nahin F. Siddiqui <nahinsiddiqui@iut-dhaka.edu>
2025-05-26 23:42:34 +0600

    change pos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -88,7 +88,7 @@ const Hero = () => {
                 </div>
                 
                 {/* Profile image with same bottom alignment */}
-                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
                   <div className="w-72 h-72 md:w-96 md:h-96 relative">
                     <img
                       src="https://i.postimg.cc/26Z6nDtg/image-2.png"
