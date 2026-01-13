7e5d16967ef6549f8aae0003d2dc9bdb158dcdb3
Nahin F. Siddiqui <nahinsiddiqui@iut-dhaka.edu>
2026-01-14 00:44:49 +0600

    3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -56,12 +56,12 @@ const Index = () => {
 
         {/* Education & Research Side-by-Side */}
         <section className="max-w-7xl mx-auto px-4 md:px-8 py-16">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
             <Education />
             <ResearchInterest />
           </div>
         </section>
-          
+
         <TechnicalSkills />
 
         <Portfolio />
